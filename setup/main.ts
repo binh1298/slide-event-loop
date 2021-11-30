67ecd4050880bcb77eb49db18f017d19811872f7
Pham Duc Binh <binh1298@gmail.com>
2021-11-30 16:02:51 +0700

    remove unnecesarry vue router setup

--- setup/main.ts
+++
@@ -1,7 +0,0 @@
-import { defineAppSetup } from "@slidev/types";
-import VueRouter from "vue-router";
-
-export default defineAppSetup(({ app, router }) => {
-  // Vue App
-  app.use(VueRouter);
-});
